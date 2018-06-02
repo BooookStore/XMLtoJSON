eb8927fc43f29ea60497fb1a6306a3118b220649
bookstore <booookstore@icloud.com>
2018-06-02 15:34:26 +0900

    feat : Element hold other elements

--- XMLElement.hs
+++ XMLElement.hs
@@ -1,18 +1,33 @@
 module XMLElement (
-    XMLElement(..)
+    Printable,
+    createXMLElement,
+    putUpXMLElement,
+    insertXMLElement,
 ) where
 
 -- Express XML Element
-data XMLElement content = 
-      Element content (XMLElement content) | None
-      deriving (Show)
+data Element content = Element content [(Element content)] deriving (Show)
+
+-- Type define string holding xmle element
+type StringElement = Element String
+
+instance Show (Element c) where
+    show (Element c []) = 
 
 -- Convert to String class
-class Convertable c where
+class Printable c where
     convert :: c -> String
 
--- Instantiate XMLElement Convertable
-instance (Show c) => Convertable (XMLElement c) where
-    convert (Element c None) = show c
-    convert (Element c elem) = show c ++ " - " ++convert elem
-    convert None = ""
+-- Instantiate XMLStructure Convertable
+instance (Show c) => Printable (Element c) where
+    convert (Element c []) = show c
+    convert (Element c xs) = show c ++ foldl (\acc x -> acc ++ show x) "" xs
+
+createXMLElement :: String -> StringElement
+createXMLElement content = Element content []
+
+putUpXMLElement :: String -> StringElement -> StringElement
+putUpXMLElement ca child = Element ca [child]
+
+insertXMLElement :: String -> StringElement -> StringElement
+insertXMLElement ca (Element cb xs) = Element cb (xs ++ [createXMLElement ca])
