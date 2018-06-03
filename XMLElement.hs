16faeaa29fe8611ab5bd307bc692753d43e5a73f
bookstore <booookstore@icloud.com>
2018-06-03 14:26:02 +0900

    feat : restructure xml element.

--- XMLElement.hs
+++ XMLElement.hs
@@ -1,33 +1,33 @@
-module XMLElement (
-    Printable,
-    createXMLElement,
-    putUpXMLElement,
-    insertXMLElement,
-) where
+module XMLElement where
+
+type Name = String
+type Content = String
 
 -- Express XML Element
-data Element content = Element content [(Element content)] deriving (Show)
+data Element = Element Name Content [Element]
 
--- Type define string holding xmle element
-type StringElement = Element String
+instance Show Element where
+    show (Element name content []) = "<" ++ name ++ ">" ++ content ++ "</" ++ name ++ ">"
+    show (Element name content xs) = "<" ++ name ++ ">" ++ content ++ "\n\t" ++ print xs ++ "\n</" ++ name ++ ">"
+        where print xs = foldl (\acc x -> acc ++ show x) "" xs
 
-instance Show (Element c) where
-    show (Element c []) = 
+-- -- Convert to String class
+-- class Printable c where
+--     convert :: c -> String
 
--- Convert to String class
-class Printable c where
-    convert :: c -> String
+-- -- Instantiate XMLStructure Convertable
+-- instance (Show c) => Printable (Element c) where
+--     convert (Element c []) = show c
+--     convert (Element c xs) = show c ++ foldl (\acc x -> acc ++ show x) "" xs
 
--- Instantiate XMLStructure Convertable
-instance (Show c) => Printable (Element c) where
-    convert (Element c []) = show c
-    convert (Element c xs) = show c ++ foldl (\acc x -> acc ++ show x) "" xs
+-- createXMLElement :: String -> Element c
+-- createXMLElement content = Element content []
 
-createXMLElement :: String -> StringElement
-createXMLElement content = Element content []
+-- putUpXMLElement :: String -> Element c -> Element c
+-- putUpXMLElement ca child = Element ca [child]
 
-putUpXMLElement :: String -> StringElement -> StringElement
-putUpXMLElement ca child = Element ca [child]
+-- insertXMLElement :: String -> Element c -> Element c
+-- insertXMLElement ca (Element cb xs) = Element cb (xs ++ [createXMLElement ca])
 
-insertXMLElement :: String -> StringElement -> StringElement
-insertXMLElement ca (Element cb xs) = Element cb (xs ++ [createXMLElement ca])
+-- printContent :: Element c -> String
+-- printContent (Element c []) = "Element has " ++ c
